330293ce063676afc2822f7b7de1406e4e02338a
ryanmosc <ryanoliveiramosc.com.098@gmail.com>
2025-12-26 16:14:01 -0300

    aaaaa

--- src/main/java/api_externa/api_externa/controller/EntidadeController.java
+++ src/main/java/api_externa/api_externa/controller/EntidadeController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/posts/penis")
+@RequestMapping("/posts")
 public class EntidadeController {
 
     @Autowired
