7c5fee9cc4c89f901291dfae1a51aaddc76bc603
ryanmosc <ryanoliveiramosc.com.098@gmail.com>
2025-12-26 16:11:37 -0300

    criei o criado

--- src/main/java/api_externa/api_externa/controller/EntidadeController.java
+++ src/main/java/api_externa/api_externa/controller/EntidadeController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/posts")
+@RequestMapping("/posts/penis")
 public class EntidadeController {
 
     @Autowired
